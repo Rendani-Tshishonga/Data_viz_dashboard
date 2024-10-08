6c2f279b05811cc75cd80963c7d538aaaf39ec2b
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-10-08 14:49:28 +0200

    to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ This module defines a base model for the metadata of our models
 # import Libraries
 from uuid import uuid4
 from datetime import datetime
+import models
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, dateTime
 
@@ -22,12 +23,24 @@ class BaseModel:
     
     def __str___(self):
         """Returns a string representation of the instance"""
-        return self.__dict__
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
     
     def save(self):
-        """updates updated_at with current time when instance is changed"""
+        """Adds and saves the current database object to the database"""
         self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
+    
+    def to_dict(self):
+        """Returns a dictionary of all the instance"""
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict[created_at].strftime("%Y-%m-%dT%H:%M:%S.%f")
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime("Y-%m-%dT%H:%M:%S.%f")
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
     
     def delete(self):
         """Deletes the current instance of storage"""
-        self.delete()
+        models.storage.delete(self)
