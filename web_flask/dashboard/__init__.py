3e02b762a1474299ef40bd01ebc8d90dd8963417
Rendani-Tshishonga <rendanijunior20@gmail.com>
2025-01-17 23:37:33 +0200

    layout

--- web_flask/dashboard/__init__.py
+++ web_flask/dashboard/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from faker import Faker
 import secrets
 
 
@@ -13,6 +14,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+faker = Faker()
 
 from dashboard import routes
 from dashboard.models import Order, Products, Shipments, Suppliers, User
