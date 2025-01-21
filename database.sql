a1f2d937d70f199b6de951e60e8ddae22f2948e0
Rendani-Tshishonga <rendanijunior20@gmail.com>
2025-01-21 14:53:46 +0200

    Design

--- database.sql
+++
@@ -1,7 +0,0 @@
--- Create a Database for the dashboard service
-
-CREATE DATABASE IF NOT EXISTS data_dev_db;
-CREATE USER IF NOT EXISTS 'data_dev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'data_dev_pwd';
-GRANT ALL PRIVILEGES ON `data_dev_db`.* TO 'data_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'data_dev'@'localhost';
-FLUSH PRIVILEGES;
--- tests/env.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-"""Load and print the environment variables"""
-
-from dotenv import load_dotenv
-
-env_list = load_doten
